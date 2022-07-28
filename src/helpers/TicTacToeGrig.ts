9e8d3c14f71415aefbbbfdcc85a781879c4df693
jacquinh <Hubert.Jacquin@stib.brussels>
2022-07-28 22:54:52 +0200

    empty grid

--- src/helpers/TicTacToeGrig.ts
+++ src/helpers/TicTacToeGrig.ts
@@ -1,8 +1,21 @@
+const htmlGrid = `
+<div class="tictactoe--container">
+  <div id="ttt-cell-1" class="cell"></div>
+  <div id="ttt-cell-2" class="cell"></div>
+  <div id="ttt-cell-3" class="cell"></div>
+  <div id="ttt-cell-4" class="cell"></div>
+  <div id="ttt-cell-5" class="cell"></div>
+  <div id="ttt-cell-6" class="cell"></div>
+  <div id="ttt-cell-7" class="cell"></div>
+  <div id="ttt-cell-8" class="cell"></div>
+  <div id="ttt-cell-9" class="cell"></div>
+</div>`
+
 export class TicTacToeGrig {
 
     parentElement? : HTMLElement
     initialise(parentElement: HTMLElement){
         this.parentElement = parentElement
-        this.parentElement.append(``)
+        this.parentElement.innerHTML = htmlGrid
     }
 }
--- src/tests/ticTacToeGrid.test.ts
+++ src/tests/ticTacToeGrid.test.ts
@@ -9,5 +9,11 @@ test("should be initialise without error", () => {
     const parent = document.createElement('div')
     ticTacToeGrig.initialise(parent)
   });
-
+  test("should be initialise and contains 9 empty cell ", () => {
+    const ticTacToeGrig = new TicTacToeGrig()
+    const parent = document.createElement('div')
+    ticTacToeGrig.initialise(parent)
+    expect(parent.querySelectorAll('.cell').length).toEqual(9)
+    expect(parent.textContent?.trim()).toEqual('')
+  });
   
